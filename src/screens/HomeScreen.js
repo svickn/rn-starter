c4eb2cd5f5e6c86968a26aec2997bf208d677528
Stacy Vicknair <svickn@gmail.com>
2021-08-18 18:40:30 -0500

    Section 4: Lesson 31

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello World!</Text>
       <Button 
         onPress={() => navigation.navigate('Components')}
         title="Go to Components Demo" />
-      <TouchableOpacity
-        onPress={() => navigation.navigate('List')}>
-        <Text>Go to List Demo</Text>
-      </TouchableOpacity>
+      <Button 
+        onPress={() => navigation.navigate('List')}
+        title="Go to List Demo" />
     </View>
   )
 };
