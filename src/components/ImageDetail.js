75de58b0f9d57cf40aa66e290ec060b1adbfe66f
Stacy Vicknair <svickn@gmail.com>
2021-08-18 21:11:40 -0500

    Section 5: Lesson 38

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, StyleSheet, View, Image } from 'react-native';
+
+const ImageDetail = ({title, image}) => {
+    return <View>
+        <Image source={image} />
+        <Text>{title}</Text>
+    </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default ImageDetail
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,8 +1,37 @@
 import React from 'react';
-import { Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View, FlatList } from 'react-native';
+import ImageDetail from '../components/ImageDetail';
 
 const ImageScreen = () => {
-    return <Text>Image Screen</Text>
+    const images = [
+        {
+            title: "Forest",
+            image: require("../../assets/forest.jpg")
+        },
+        {
+            title: "Beach",
+            image: require("../../assets/beach.jpg")
+        },
+        {
+            title: "Mountain",
+            image: require("../../assets/mountain.jpg")
+        },
+    ]
+
+    function renderImageDetail({item}) {
+        return <ImageDetail title={item.title} image={item.image} />
+    }
+
+    return (
+        <View>
+            <FlatList
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item) => item.title}   
+                data={images} 
+                renderItem={renderImageDetail} 
+            />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({})
