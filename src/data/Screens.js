2f29522dcbc2bce329cec9e8a7484222bed9445b
Stacy Vicknair <svickn@gmail.com>
2021-08-18 22:28:33 -0500

    Section 6: Lesson 48

--- src/data/Screens.js
+++ src/data/Screens.js
@@ -3,13 +3,15 @@ import ComponentsScreen from "../screens/ComponentsScreen";
 import ListScreen from "../screens/ListScreen";
 import ImageScreen from "../screens/ImageScreen";
 import CounterScreen from "../screens/CounterScreen";
+import ColorsScreen from "../screens/ColorsScreen";
 
 const screens = {
     Home: HomeScreen,
     Components: ComponentsScreen,
     List: ListScreen,
     Image: ImageScreen,
-    Counter: CounterScreen
+    Counter: CounterScreen,
+    Colors: ColorsScreen
   }
 
   export default screens
---
+++ src/screens/ColorsScreen.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Text, StyleSheet, View, Button, FlatList } from 'react-native';
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const ColorsScreen = () => {
+    const [colors, setColors] = React.useState([])
+
+    function handleAddColor() {
+        setColors([...colors, `rgb(${getRandomInt(255)},${getRandomInt(255)},${getRandomInt(255)})`])
+    }
+
+    function renderColor({item}) {
+        const size = 200
+        return <View style={{backgroundColor: item, height: size, width: size, marginTop: 10}}><Text>{item}</Text></View>
+    }
+
+    return <View>
+        <Button title="Add a Color" onPress={handleAddColor} />
+        <FlatList
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item) => item}   
+                data={colors} 
+                renderItem={renderColor} 
+            />
+    </View>
+}
+
+const styles = StyleSheet.create({})
+
+export default ColorsScreen
